165f9ca5bf551a6bf7ba51fd8f809afc90ed882b
MrJustMeDahl <nico.dahl93@gmail.com>
2023-03-21 19:53:31 +0100

    Implemented Entity classes Cupcake, CupcakePart, CupcakeBase, CupcakeTopping, Order and ShoppingBasket. Updated UML where necessary.

---
+++ src/main/java/dat/backend/model/entities/CupcakeBase.java
@@ -0,0 +1,8 @@
+package dat.backend.model.entities;
+
+public class CupcakeBase extends CupcakePart{
+
+    public CupcakeBase(String flavor, float price){
+        super(flavor, price);
+    }
+}
---
+++ src/main/java/dat/backend/model/entities/CupcakeTopping.java
@@ -0,0 +1,8 @@
+package dat.backend.model.entities;
+
+public class CupcakeTopping extends CupcakePart {
+
+    public CupcakeTopping(String flavor, float price){
+        super(flavor, price);
+    }
+}
