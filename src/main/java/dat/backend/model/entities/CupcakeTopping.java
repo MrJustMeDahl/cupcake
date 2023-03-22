0765120ea4df51ab9cccba9d6a668ae7275a9e83
MrJustMeDahl <nico.dahl93@gmail.com>
2023-03-22 13:40:31 +0100

    Completed orderMapper

--- src/main/java/dat/backend/model/entities/CupcakeTopping.java
+++ src/main/java/dat/backend/model/entities/CupcakeTopping.java
@@ -8,4 +8,8 @@ public class CupcakeTopping extends CupcakePart {
         super(flavor, price);
         this.toppingID = toppingID;
     }
+
+    public int getToppingID(){
+        return toppingID;
+    }
 }
--- src/main/java/dat/backend/model/persistence/OrderMapper.java
+++ src/main/java/dat/backend/model/persistence/OrderMapper.java
@@ -13,11 +13,12 @@ import java.util.List;
 class OrderMapper {
 
     static Order createOrder(Cupcake cupcake, int userId, ConnectionPool connectionPool) throws DatabaseException {
-        String sql = "INSERT INTO cupcake.order (price, user_id) VALUES (?, ?)";
+        String sqlOrder = "INSERT INTO cupcake.order (price, user_id) VALUES (?, ?)";
         int orderId;
-
+        List<Cupcake> cupcakes = new ArrayList<>();
+        cupcakes.add(cupcake);
         try (Connection connection = connectionPool.getConnection()) {
-            try (PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            try (PreparedStatement ps = connection.prepareStatement(sqlOrder, Statement.RETURN_GENERATED_KEYS)) {
                 ps.setFloat(1, cupcake.getFullPrice());
                 ps.setInt(2, userId);
                 ps.executeUpdate();
@@ -32,9 +33,18 @@ class OrderMapper {
         } catch (SQLException ex) {
             throw new DatabaseException(ex, "Error creating order. Something went wrong with the database");
         }
-
-
-        return ;
+        String sqlCupcake = "INSERT INTO cupcake.cupcake (orderId, cupcakebaseId, cupcaketoppingId) VALUES (?, ?, ?)";
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sqlCupcake)) {
+                ps.setInt(1, orderId);
+                ps.setInt(2, cupcake.getBase().getBaseID());
+                ps.setInt(3, cupcake.getTopping().getToppingID());
+                ps.executeUpdate();
+            }
+        } catch (SQLException ex) {
+            throw new DatabaseException(ex, "Error creating order. Something went wrong with the database");
+        }
+        return new Order(orderId, userId, cupcakes, false, false);
     }
 
     static Order getOrderByOrderId(int orderId, ConnectionPool connectionPool){
@@ -54,7 +64,6 @@ class OrderMapper {
                     userId = rs.getInt("userId");
                     isPaid = rs.getBoolean("isPaid");
                     isOrdered = rs.getBoolean("isOrdered");
-
                 }
             } catch(SQLException e){
                 e.printStackTrace();
@@ -62,21 +71,10 @@ class OrderMapper {
         } catch(SQLException e){
             e.printStackTrace();
         }
-        String sqlCupcakebase = "SELECT * FROM cupcake.cupcakeview WHERE orderId = ?";
         try(Connection connection = connectionPool.getConnection()){
-            try(PreparedStatement ps = connection.prepareStatement(sqlCupcakebase)){
-                ps.setInt(1, orderId);
-                ResultSet rs = ps.executeQuery();
-                while(rs.next()){
-                    CupcakeBase cupcakeBase = new CupcakeBase(rs.getString("baseflavor"), rs.getFloat("basePrice"),rs.getInt("baseId"));
-                    CupcakeTopping cupcakeTop = new CupcakeTopping(rs.getString("toppingflavor"), rs.getFloat("toppingprice"), rs.getInt("toppingId"));
-                    cupcakes.add(new Cupcake(cupcakeTop, cupcakeBase));
-                }
-            } catch(SQLException e){
-                e.printStackTrace();
-            }
-        } catch(SQLException e){
-            e.printStackTrace();
+            cupcakes = getCupcakesForOrder(id, connection);
+        } catch (SQLException e){
+
         }
         order = new Order(id, userId, cupcakes, isPaid, isOrdered);
         return order;
@@ -84,30 +82,46 @@ class OrderMapper {
 
     static List<Order> getOrdersByUserId(int userId, ConnectionPool connectionPool) throws DatabaseException {
         List<Order> orders = new ArrayList<>();
-        String sql = "SELECT orders.id AS order_id, order_items.product_id AS product_id FROM orders INNER JOIN order_items ON orders.id = order_items.order_id WHERE orders.user_id = ?";
-
-        try (Connection connection = connectionPool.getConnection()) {
-            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        String sqlOrder = "SELECT * FROM cupcake.order WHERE userId = ?";
+        int id = 0;
+        List<Cupcake> cupcakes = new ArrayList<>();
+        boolean isPaid = false;
+        boolean isOrdered = false;
+        try(Connection connection = connectionPool.getConnection()){
+            try(PreparedStatement ps = connection.prepareStatement(sqlOrder)){
                 ps.setInt(1, userId);
+                cupcakes = getCupcakesForOrder(id, connection);
                 ResultSet rs = ps.executeQuery();
-
-                while (rs.next()) {
-                    int orderId = rs.getInt("order_id");
-                    int productId = rs.getInt("product_id");
-                    Order order = orders.stream().filter(o -> o.getId() == orderId).findFirst().orElse(null);
-
-                    if (order == null) {
-                        order = new Order(orderId, userId, new ArrayList<>());
-                        orders.add(order);
-                    }
-                    order.getProductIds().add(productId);
+                while(rs.next()){
+                    id = rs.getInt("orderId");
+                    isPaid = rs.getBoolean("isPaid");
+                    isOrdered = rs.getBoolean("isOrdered");
+                    orders.add(new Order(id, userId, cupcakes, isOrdered, isPaid));
                 }
+            } catch(SQLException e){
+                e.printStackTrace();
             }
-        } catch (SQLException ex) {
-            throw new DatabaseException(ex, "Error retrieving orders. Something went wrong with the database");
+        } catch(SQLException e) {
+            e.printStackTrace();
         }
-
         return orders;
+    }
+
+    private static List<Cupcake> getCupcakesForOrder(int orderId, Connection connection){
+        String sqlCupcakebase = "SELECT * FROM cupcake.cupcakeview WHERE orderId = ?";
+        List<Cupcake> cupcakes = new ArrayList<>();
+        try(PreparedStatement ps = connection.prepareStatement(sqlCupcakebase)){
+            ps.setInt(1, orderId);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                CupcakeBase cupcakeBase = new CupcakeBase(rs.getString("baseflavor"), rs.getFloat("basePrice"),rs.getInt("baseId"));
+                CupcakeTopping cupcakeTop = new CupcakeTopping(rs.getString("toppingflavor"), rs.getFloat("toppingprice"), rs.getInt("toppingId"));
+                cupcakes.add(new Cupcake(cupcakeTop, cupcakeBase));
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return cupcakes;
     }
 }
 
