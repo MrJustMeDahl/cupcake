05dab739e5b2b8fa3007a4fe86c60b9b2bc65540
MrJustMeDahl <nico.dahl93@gmail.com>
2023-03-22 10:39:07 +0100

    Updated OrderFacade

--- src/main/java/dat/backend/model/persistence/OrderFacade.java
+++ src/main/java/dat/backend/model/persistence/OrderFacade.java
@@ -2,24 +2,17 @@ package dat.backend.model.persistence;
 
 import dat.backend.model.entities.Order;
 import dat.backend.model.exceptions.DatabaseException;
-import dat.backend.model.persistence.ConnectionPool;
-import dat.backend.model.persistence.OrderMapper;
+
 
 import java.util.List;
 
 public class OrderFacade {
 
-    private ConnectionPool connectionPool;
-
-    public OrderFacade() {
-        connectionPool = ConnectionPool.getInstance();
-    }
-
-    public Order createOrder(int userId, List<Integer> productIds) throws DatabaseException {
+    public static Order createOrder(int userId, List<Integer> productIds, ConnectionPool connectionPool) throws DatabaseException {
         return OrderMapper.createOrder(userId, productIds, connectionPool);
     }
 
-    public List<Order> getOrdersByUserId(int userId) throws DatabaseException {
+    public static List<Order> getOrdersByUserId(int userId, ConnectionPool connectionPool) throws DatabaseException {
         return OrderMapper.getOrdersByUserId(userId, connectionPool);
     }
 
--- src/main/java/dat/backend/model/persistence/OrderMapper.java
+++ src/main/java/dat/backend/model/persistence/OrderMapper.java
@@ -73,3 +73,4 @@ class OrderMapper {
         return orders;
     }
 }
+
